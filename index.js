9566f803745c5153195f377768889c3e456003cc
Nils Bentlage <nils.bentlage@googlemail.com>
2021-07-15 20:58:34 +0200

    updated some stuff

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-let targetValue, startAt, percentage;
+let liveValue, targetValue, startAt, percentage;
+
+let setProgress = function(value) {
+	document.getElementById('meine-status-bar').style.backgroundSize = value + '%';
+};
 
 window.addEventListener('onWidgetLoad', function (obj) {
     let data=obj["detail"]["session"]["data"];
- //   let recents=obj["detail"]["recents"];
- //   let currency=obj["detail"]["currency"];
- //   let channelName=obj["detail"]["channel"]["username"];
- //   let apiToken=obj["detail"]["channel"]["apiToken"];
     let fieldData=obj["detail"]["fieldData"];
-	let liveValue=data["follower-total"]["count"];
-  
+  	liveValue=data["subscriber-points"]["amount"];
     targetValue=fieldData.targetValue;
     startAt=fieldData.startAt;
   	percentage = ((liveValue - startAt) / (targetValue - startAt)) * 100;
-  
- 	document.getElementById('meine-status-bar').style.backgroundSize = percentage + '%';
+	setProgress(percentage);  
 });
 
 window.addEventListener('onEventReceived', function (obj) {
-  	if (obj.detail.listener == "follower-latest") {
-    	let liveValue = obj.detail.event.amount;
+	 if (obj.detail.listener == "subscriber-latest") {
+		liveValue ++;
         percentage = ((liveValue - startAt) / (targetValue - startAt)) * 100;
-		document.getElementById('meine-status-bar').style.backgroundSize = percentage + '%';
-    }
+		setProgress(percentage);}
 });
